5a8e138fd2079a5c9095932c9aab4bc58d0cd1d8
mellow-hype <25216562+mellow-hype@users.noreply.github.com>
2024-01-15 18:23:31 -0800

    fix kernel script

--- init-kernel.sh
+++ init-kernel.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Create a new buildroot kernel build
+# Create a new buildroot kernel module build
 
 if [ "$#" -ne 2 ]; then
     echo "usage: $0 <name> <buildroot-version>"
@@ -11,13 +11,12 @@ BUILDROOT_VERSION=$2
 BUILDROOT_NAME="buildroot-$BUILDROOT_VERSION"
 BUILDROOT_PATH="/tmp/$BUILDROOT_NAME"
 
-
-if [ -z "${BR2_EXTERNAL+x}" ]; then
-    BREXT=""
-    echo "no BR2_EXTERNAL env var found, not setting this for the buildroot init"
-else
-    BREXT="BR2_EXTERNAL=$BR2_EXTERNAL"
-fi
+# buildroot output directory
+BROUT="${PWD}/${NAME}/build"
+# buildroot external tree directory
+EXT_PATH="${PWD}/${NAME}/ext-tree"
+# the base dir where actual code is stored
+SRCBASE=$NAME/src
 
 # download the buildroot archive now if it's not in /tmp so we can bail early if that fails too
 if [ ! -d $BUILDROOT_PATH ]; then
@@ -29,18 +28,53 @@ if [ ! -d $BUILDROOT_PATH ]; then
     fi
 fi
 
+### SET UP THE BUILDROOT EXTERNAL TREE STRUCTURE
+# create the base dirs for the build
+mkdir -p $BROUT
+mkdir -p $EXT_PATH
 
-mkdir -p ./builds
-ODIR=./builds/$NAME
-mkdir -p $ODIR/build
-cp configs/*FRAG $ODIR/.
-cp -a $BUILDROOT_PATH $ODIR/.
-cp template/build.sh $ODIR/.
-cp template/qemu-run.sh $ODIR/.
+# copy over the template ext-tree directory
+cp -a template/ext-tree $NAME/.
+# replace lowercase instances
+# sed -i "s/pkgname/${NAME}/g" $EXT_PATH/Config.in
+# sed -i "s/pkgname/${NAME}/g" $EXT_PATH/external.desc
+# sed -i "s/pkgname/${NAME}/g" $EXT_PATH/external.mk
+# # replace uppercase instances
+# sed -i "s/PKGNAME/${NAME^^}/g" $EXT_PATH/Config.in
+# sed -i "s/PKGNAME/${NAME^^}/g" $EXT_PATH/external.desc
+# sed -i "s/PKGNAME/${NAME^^}/g" $EXT_PATH/external.mk
 
-cd $ODIR/$BUILDROOT_NAME
-make O=../build "$BREXT" qemu_x86_64_defconfig
+# # rename the package and pkg files in the ext-tree packages dir from the
+# # template values
+# PACKDIR="$EXT_PATH/package/$NAME"
+# mv $EXT_PATH/package/pkgname $PACKDIR
+# mv $PACKDIR/pkgname.mk $PACKDIR/$NAME.mk
+# # replace lowercase instances
+# sed -i "s/pkgname/${NAME}/g" $PACKDIR/$NAME.mk
+# sed -i "s/pkgname/${NAME}/g" $PACKDIR/Config.in
+# # replace uppercase instances
+# sed -i "s/PKGNAME/${NAME^^}/g" $PACKDIR/$NAME.mk
+# sed -i "s/PKGNAME/${NAME^^}/g" $PACKDIR/Config.in
 
-cd ../build
+# copy in the build script (just runs make in the build dir, assumes everything else has been set)
+cp template/build.sh $NAME/.
+cp template/qemu-run.sh $NAME/.
+chmod +x $NAME/build.sh
+chmod +x $NAME/qemu-run.sh
+
+# copy config fragments and the buildroot tree into the project dir
+cp configs/*FRAG $NAME/.
+cp -a $BUILDROOT_PATH $NAME/$BUILDROOT_NAME
+cd $NAME/$BUILDROOT_NAME
+
+# stick to using relative paths so the project dir can be relocated without breaking builds (mostly)
+make O=../build BR2_EXTERNAL=../ext-tree qemu_x86_64_defconfig
+
+# move back out to the build dir to update the generated .config with the fragments
+cd $BROUT
 cat ../defconfig-FRAG >> .config
 make olddefconfig
+
+# kick off menuconfig to allow user to enable the building of the custom package
+make menuconfig
+
