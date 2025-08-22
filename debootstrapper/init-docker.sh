292567d8f86f2c9ec658bb9e769a450e28ffe40f
mellow-hype <25216562+mellow-hype@users.noreply.github.com>
2025-08-22 02:04:09 -0700

    add cross-compile inits

--- debootstrapper/init-docker.sh
+++ debootstrapper/init-docker.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 SRCDIR="./src"
 TAG="debian-bootstrapper"
-DFILE="src/debootstrapper.Dockerfile"
+TAG_CROSS="debian-bootstrapper-cross"
+DFILE="$SRCDIR/debootstrapper.Dockerfile"
+DFILE_CROSS="$SRCDIR/debootstrapper-cross.Dockerfile"
 
-echo "[*] building docker image"
+echo "[*] building docker image for standard version"
 docker build -t "${TAG}" -f "${DFILE}" "${SRCDIR}"
+
+echo "[*] building docker image for cross-compile version"
+docker build -t "${TAG_CROSS}" -f "${DFILE_CROSS}" "${SRCDIR}"
 
 echo "[*] creating symlinks to util scripts in ~/.local/bin"
 mkdir -p $HOME/.local/bin
 test -f $HOME/.local/bin/debootstrap-dock || ln -s $PWD/util/debootstrapper-dock.sh $HOME/.local/bin/debootstrap-dock
+test -f $HOME/.local/bin/debootstrap-cross-dock || ln -s $PWD/util/debootstrapper-cross-dock.sh $HOME/.local/bin/debootstrap-cross-dock
 test -f $HOME/.local/bin/debootstrap-rebuild-img || ln -s $PWD/util/rebuild-img.sh $HOME/.local/bin/debootstrap-rebuild-img
