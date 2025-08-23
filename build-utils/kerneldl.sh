42b8f1d96f6046fac6b39cae336dee52ad3139a6
mellow-hype <25216562+mellow-hype@users.noreply.github.com>
2025-08-22 21:46:30 -0700

    minor changes

--- build-utils/kerneldl.sh
+++ build-utils/kerneldl.sh
@@ -5,12 +5,15 @@ if [ -z $1 ]; then
     exit 1
 fi
 
+OUTDIR=$PWD
+if [ ! -z $2 ]; then
+    echo "Using output directory: $2"
+    OUTDIR=$2
+fi
+
 KERNEL_FULL="$1"
 KERNEL_SHORT=$(echo "$KERNEL_FULL" | cut -d '.' -f 1)
 KERNEL_URL="https://www.kernel.org/pub/linux/kernel/v$KERNEL_SHORT.x/linux-$KERNEL_FULL.tar.gz"
 
 echo "kernel: $KERNEL_FULL"
-echo "short: $KERNEL_SHORT.x"
-
-wget "${KERNEL_URL}"
-
+wget "${KERNEL_URL}" -P $OUTDIR/
