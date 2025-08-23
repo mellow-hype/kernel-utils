6a8b866d8ca12ab90e2e73124a0b9714f2ff614a
mellow-hype <25216562+mellow-hype@users.noreply.github.com>
2025-08-23 00:02:36 -0700

    rename qemu run script and add port config option (x86_64)

--- qemu-run-x86_64.sh
+++ qemu-run-x86_64.sh
@@ -5,6 +5,13 @@ if [ "$#" != 2 ]; then
     exit 1
 fi
 
+if [ ! -z ${QEMU_SSH_PORT} ]; then
+    echo "[*] Using port ${QEMU_SSH_PORT} for SSH forwarding to localhost"
+else
+    echo "[*] Using default port 10021 for SSH forwarding to localhost"
+    QEMU_SSH_PORT="10021"
+fi
+
 KERNEL="$1"
 ROOTFS="$2"
 
@@ -14,7 +21,7 @@ qemu-system-x86_64 \
         -kernel "$KERNEL" \
         -append "console=ttyS0 root=/dev/sda earlyprintk=serial panic_on_oops=0 net.ifnames=0 nokaslr" \
         -drive file="$ROOTFS",format=raw \
-        -net user,host=10.0.2.10,hostfwd=tcp:127.0.0.1:10021-:22 \
+        -net user,host=10.0.2.10,hostfwd=tcp:127.0.0.1:"$QEMU_SSH_PORT"-:22 \
         -net nic,model=e1000 \
         -enable-kvm \
         -nographic
